003c05bca14644e6e69e94942de0f99eaf53dbad
dongwei1125 <2465204327@qq.com>
2024-09-08 22:36:48 +0800

    add: 新增服务配置文件

--- server/app.js
+++ server/app.js
@@ -4,16 +4,18 @@ const chalk = require('chalk')
 const { program } = require('commander')
 
 const projectRouter = require('./routes/project')
+const { PORT } = require('./server.config')
 
 const app = express()
-const { port } = program.option('-p, --port <number>', 'port number', 8800).parse(process.argv).opts()
+const opts = program.option('-p, --port <number>', 'port number', PORT).parse(process.argv).opts()
+const port = opts.port
 
 app.use(express.static(path.join(__dirname, 'public')))
 
 app.use('/api/project', projectRouter)
 
 app.listen(port, () => {
-  console.log(
-    '\n\nServer started on:\n' + '  - Local: ' + chalk.blueBright(`http://localhost:${port}/`) + '\n\n\n',
-  )
+  console.log()
+  console.log('  Express server started on:')
+  console.log(`  - Local:   ${chalk.cyan(`http://localhost:${port}/`)}`)
 })
